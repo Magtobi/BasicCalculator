ac3c01fc2d67faf50a59e0e7093b41851af3dbc8
Student <darrienconnor97@yahoo.com>
2023-10-05 12:49:53 -0400

    first commit

---
+++ src/main/java/com/pluarlsight/BasicCalculator.java
@@ -0,0 +1,35 @@
+package com.pluarlsight;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = myScanner.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = myScanner.nextInt();
+
+        myScanner.nextLine();
+        System.out.println("Possible Calculations ");
+        System.out.print("(A)dd ");
+        System.out.print("(S)ubtract ");
+        System.out.print("(M)ultiply ");
+        System.out.print("(D)ivide ");
+
+        System.out.print("Please select an option ");
+        myScanner.nextLine();
+        System.out.println("5 + 12");
+
+
+
+
+
+
+
+
+
+
+    }
+}
